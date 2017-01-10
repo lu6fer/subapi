92fe8c749843905663333847da89b7c11100fb9a
SXRK6690 <fhunkeler.ext@orange.com>
2017-01-10 14:24:00 +0100

    onpeshift

--- migrate-database.sh
+++ migrate-database.sh
@@ -2,7 +2,7 @@
 set -e 
 
 echo "Migrating database 'php artisan migrate --force'..."
-php --version
-php /opt/app-root/src/composer.phar install
+#php --version
+#php /opt/app-root/src/composer.phar install
 php artisan migrate
 php artisan db:seed
